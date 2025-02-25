5c0fb6f044bb45ce6c7f45fb34973b9e6de083ac
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2025-02-25 16:56:47 +0530

    fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -52,7 +52,13 @@ const HeroWithNavbar = () => {
               />
             </div>
           ) : (
-            <video className="w-full h-95 object-cover" autoPlay muted playsInline key={heroVideo}>
+            <video
+              className="w-full h-95 object-cover"
+              autoPlay
+              muted
+              playsInline
+              key={heroVideo}
+            >
               <source src={heroVideo} type="video/mp4" />
             </video>
           )}
@@ -62,22 +68,24 @@ const HeroWithNavbar = () => {
 
         {/* <==removed not required now but when required we could call  */}
 
-        <div className="bg-gray-300 text-white py-3 px-4 text-center w-full">
-           
-            <p className="text-sm mb-1">
-              Get iPhone 16 Pro from just ₹4912.00/mo. for up to 24 mo.* with No Cost EMI and instant cashback.<br />
-              <a href="#" className="text-blue ml-2 hover:underline">Buy &gt;</a>
-            </p>
+        <div className="bg-gray-300 text-white py-3 px-4 text-center w-full relative z-20">
+          <p className="text-[0.9rem] px-3">
+            Get iPhone 16 Pro from just ₹4912.00/mo. for up to 24 mo.* with No
+            Cost EMI and instant cashback.
+            <br className="block sm:hidden" />
+            <a href="#" className="text-blue ml-2 hover:underline">
+              Buy &gt;
+            </a>
+          </p>
         </div>
 
         <div className="relative h-full w-full flex-center flex-col z-10">
           <div
             className="flex flex-col items-center"
             style={{
-              marginTop: isSmallScreen ? '-80px' : '0'
+              marginTop: isSmallScreen ? "-80px" : "0",
             }}
           >
-
             <p
               id="hero"
               className="hero-title  md:text-3xl font-semibold mb-4"
@@ -89,7 +97,7 @@ const HeroWithNavbar = () => {
               id="image"
               src={appleIntelligenceImage}
               alt="Built for Apple Intelligence"
-              className="w-full max-w-sm md:max-w-4xl -mt-12 md:-mt-48"
+              className="w-full max-w-sm md:max-w-4xl -mt-24 md:-mt-48"
               style={{ opacity: isSmallScreen ? 1 : 0 }}
             />
           </div>
@@ -99,10 +107,13 @@ const HeroWithNavbar = () => {
             className="flex flex-col items-center mt-4 md:mt-0"
             style={{
               opacity: isSmallScreen ? 1 : 0,
-              transform: isSmallScreen ? "none" : "translateY(80px)"
+              transform: isSmallScreen ? "none" : "translateY(80px)",
             }}
           >
-            <a href="#highlights" className="btn bg-white text-black buy-btn rounded-full px-6 py-2 text-md font-medium mb-4">
+            <a
+              href="#highlights"
+              className="btn bg-white text-black buy-btn rounded-full px-6 py-2 text-md font-medium mb-4"
+            >
               Buy
             </a>
             <p className="text-gray-200 text-lg md:text-xl">From ₹119900.00*</p>
